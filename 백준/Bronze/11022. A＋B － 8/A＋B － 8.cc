8684f3b208db66515659a363897821aa24bca57a
t-geon <88877637+t-geon@users.noreply.github.com>
2022-05-11 17:29:55 +0900

    [Bronze III] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int count, a, b;
+	cin >> count;
+	for (int i = 1; i <= count; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a <<" + "<< b <<" = "<<a+b << "\n";
+	}
+
+	return 0;
+}
