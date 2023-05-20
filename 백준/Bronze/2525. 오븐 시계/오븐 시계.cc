9add84f45bffffe3ce83fc191a78f24b569d75c5
t-geon <88877637+t-geon@users.noreply.github.com>
2023-05-21 08:43:31 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -3,22 +3,13 @@ using namespace std;
 
 int main() {
 	int h, m, t;
-	cin >> h >> m;	//시작 시간, 분
-	cin >> t;	//오븐구이 하는 시간
-
-	if (m + t >= 60) {	//시간 변경 되는 경우
-		int uh = (m + t) / 60;	//증가하는 시간
-		m = (m + t) % 60;	//변경후 분
-		int ch = h + uh;
-
-		if (ch>=24) { h = -(24-ch); }
-		else { h = ch; }
-	}
-	else {	//시간 변경 안되는 경우
-		m += t;
-	}
-
-	cout << h << " " << m;
-
+	cin >> h >> m;
+	cin >> t;
+	
+	h += (m + t) / 60;
+	m = (m + t) % 60;
+	h %= 24;
+	printf("%d %d", h, m);
+	
 	return 0;
 }
